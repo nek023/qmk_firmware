6136ee7d5074159edca90cebda89476cb331a57d
Katsuma Tanaka <questbeat@gmail.com>
2023-09-24 23:36:36 +0900

    Update keymap

--- keyboards/keyball/keyball44/keymaps/custom/keymap.c
+++ keyboards/keyball/keyball44/keymaps/custom/keymap.c
@@ -338,15 +338,15 @@ void oledkit_render_info_user(void) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOTE ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_ENT   ,
-    KC_LALT  , KC_LGUI  ,        KC_SPC , LT(2, KC_LNG2) , KC_ENT   ,                                        KC_BSPC  , LT(1, KC_LNG1) , _______ ,       _______  , KC_RCTL
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_GRV   ,
+    KC_LALT  , KC_LGUI  ,       KC_LGUI , LT(1, KC_LNG2) , KC_ENT   ,                                        KC_SPC   , LT(2, KC_LNG1) , _______ ,       _______  , KC_MCTL
   ),
 
   [1] = LAYOUT_universal(
-    _______  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                        KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                        KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , _______  , _______  ,
+    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                        KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , KC_MINS  , KC_EQL   , KC_BSLS  , KC_LBRC  , KC_RBRC  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                        _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  ,            _______  , _______  , _______  ,                                        _______  , _______  , _______  ,            _______  , _______
   ),
